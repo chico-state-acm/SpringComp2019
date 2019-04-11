0b2c35cf05ce260169df221f44a40f4a12e15b87
Tad Hardesty <tad@platymuus.com>
2019-04-11 14:47:20 -0700

    Add webpage build script, division 1 and 2 listing pages

---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+function handle() {
+    echo $1
+    mkdir -p $2/$(dirname $1)
+
+    TITLE=$(head -n1 $1 | python3 -c 'print(input().lstrip("# "))')
+
+    (
+        cat <<EOD
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>$TITLE</title>
+<meta charset="utf-8"/>
+<style>
+main {
+    margin: auto;
+    width: 80ch;
+}
+</style>
+</head>
+<body>
+<main>
+EOD
+        markdown $1
+        cat <<EOD
+</main>
+</body>
+</html>
+EOD
+    ) > $2/$(dirname $1)/index.html
+}
+
+# doesn't handle Wacky Filenames, not my problem
+rm -r _site
+handle README.md _site
+cd problems
+find . -name '*.md' | while read X; do
+    handle $X ../_site
+done
+find . -name '*.png' -or -name '*.jpg' | while read X; do
+    echo "image: $X"
+    cp $X ../_site/$X
+done
+
---
+++ problems/Division1/listing.md
@@ -0,0 +1,12 @@
+# Division One
+
+1. [String Theory](/StringTheory)
+1. [Satellite Schedule](/SatelliteSchedule)
+1. [Three Value Sum](/ThreeValueSum)
+1. [Grab Bag](/GrabBag)
+1. [Render Unto Atbash](/RenderUntoAtbash)
+1. [Singleton](/Singleton)
+1. [Tidy Numbers](/TidyNumbers)
+1. [Server Farm](/ServerFarm)
+1. [Persistent Numbers](/Persistent)
+
