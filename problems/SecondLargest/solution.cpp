f684b84375b0d1fe98f3dffac6d2fac26ba6e2f1
Robert Powers <rbert3p@gmail.com>
2019-09-20 16:57:35 -0700

    I made it better

--- problems/SecondLargest/solution.cpp
+++ problems/SecondLargest/solution.cpp
@@ -8,12 +8,12 @@ int main()
 {
   int size;
   cin >> size;
-  vector<int> A(0);
+  vector<int> A(size);
   int temp;
   for(int i = 0; i < size; i++)
   {
     cin >> temp;
-    A.push_back(temp);
+    A[i] = temp;
   }
   sort(A.begin(), A.end());
   cout << A[A.size() - 2] << endl;
